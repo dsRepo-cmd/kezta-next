1cf8e6729eca2d0bbbcfb2a19ba551e166033288
ValpSerg <Ghostnord@gmail.com>
2024-05-26 16:53:10 +0300

    Page screen

---
+++ src/containers/Page/Page.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from "react";
+import Header from "../Header/Header";
+import Image from "next/image";
+import Navigation from "../Navigation/Navigation";
+import { NavigationName } from "@/data/navigation";
+import Label from "@/components/Label/Label";
+import FollowLink from "@/components/FollowLink/FollowLink";
+import Divider from "@/components/Divider/Divider";
+
+interface PageProps {
+  children: ReactNode;
+  naigation?: NavigationName;
+}
+export default function Page({ children, naigation }: PageProps) {
+  return (
+    <div className=" relative flex flex-col min-h-screen  bg-black overflow-hidden">
+      <Header>
+        <Image src={"/logo.svg"} alt="logo" width={100} height={50} />
+        <Navigation path={naigation} />
+      </Header>
+      <Label />
+      <FollowLink />
+      <main className=" mx-auto w-[70%] flex gap-5 pr-40  ">
+        <Divider />
+        {children}
+      </main>
+
+      <div className=" absolute bottom-[150px] right-[50px] rotate-[270deg] flex justify-center items-center ">
+        <span className=" text-grayLight">©2024</span>
+      </div>
+    </div>
+  );
+}
