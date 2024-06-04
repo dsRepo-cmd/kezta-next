0e40ac23ad66b51737db3b6f1a39d43304d3f259
ValpSerg <Ghostnord@gmail.com>
2024-06-04 19:01:26 +0300

    Portfolio restr

---
+++ src/containers/PortfolioContent/PortfolioContent.tsx
@@ -0,0 +1,49 @@
+import { PortfolioContent, PortfolioView } from "@/data/portfolioContent";
+import Image from "next/image";
+import { memo } from "react";
+
+interface PortfolioContentProps {
+  view: PortfolioView;
+  filteredContent: PortfolioContent[];
+}
+
+function PortfolioImageContent({
+  view,
+  filteredContent,
+}: PortfolioContentProps) {
+  return (
+    <div
+      className={`duration-500 ${
+        view === PortfolioView.GRID
+          ? "grid grid-cols-3 gap-8"
+          : "flex flex-col gap-10"
+      }`}
+    >
+      {filteredContent.map((item) =>
+        view === PortfolioView.GRID ? (
+          <Image
+            key={item.id}
+            src={item.imageSquard}
+            alt={item.image}
+            width={920}
+            height={325}
+            className="object-contain"
+            priority
+          />
+        ) : (
+          <Image
+            key={item.id}
+            src={item.image}
+            alt={item.image}
+            width={920}
+            height={325}
+            className="object-contain"
+            priority
+          />
+        )
+      )}
+    </div>
+  );
+}
+
+export default memo(PortfolioImageContent);
