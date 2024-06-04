eeadf336d828ea8e1c86ddd62964c8c0b78d76bf
ValpSerg <Ghostnord@gmail.com>
2024-06-04 19:46:04 +0300

    Skeleton pixel edit

--- src/app/stories/loading.tsx
+++ src/app/stories/loading.tsx
@@ -3,8 +3,8 @@ import Skeleton from "@/components/Skeleton/Skeleton";
 export default function LoadingStories() {
   return (
     <>
-      <Skeleton width={"50%"} height={80} />
-      <Skeleton width={"100%"} height={80} />
+      <Skeleton width={"50%"} height={60} />
+      <Skeleton width={"100%"} height={60} />
 
       <Skeleton width={"100%"} height={400} className=" max-w-[900px]" />
       <Skeleton width={"50%"} height={50} />
